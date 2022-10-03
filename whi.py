855265061b38e7baa7992764bc976c151c4f43bb
Kamaree Robinson <kamareerobinson@gmail.com>
2022-10-03 17:54:56 -0400

    'hw'

---
+++ whi.py
@@ -0,0 +1,12 @@
+class User():
+    # def __init__(self):
+    #     self.get = ''
+    def get_string(get):
+        return input("Give me a message")
+            
+    def print_string(get):
+        print(User.get_string(get).upper())
+
+
+Kamaree = User()
+Kamaree.print_string()
